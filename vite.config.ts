f62062cbc286915bf5eaa8c233d477742728a076
Konstantin Romanovskiy <konstantinromanovskiy@Konstantins-MacBook-Pro.local>
2025-03-06 00:24:04 -0800

    changed short name PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Автоматическое обновление Service Worker
+      registerType: 'autoUpdate',
       devOptions: {
-        enabled: true, // Включить PWA в режиме разработки
+        enabled: true,
       },
       manifest: {
         name: 'ownRestaurantPWA',
-        short_name: 'PWA',
+        short_name: 'OwnRestaurant',
         id: '/',
         scope: '/',
         start_url: '/own-restaurant-pwa/',
